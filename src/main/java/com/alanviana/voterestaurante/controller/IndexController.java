984a8c8d37eba18b8fd376d61a17e62720f93e48
Alan Luiz Viana <alanlviana@hotmail.com>
2016-06-16 20:11:55 -0300

    Vraptor no index.

--- src/main/java/com/alanviana/voterestaurante/controller/IndexController.java
+++ src/main/java/com/alanviana/voterestaurante/controller/IndexController.java
@@ -3,6 +3,7 @@ package com.alanviana.voterestaurante.controller;
 import javax.inject.Inject;
 
 import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Result;
 import br.com.caelum.vraptor.view.Results;
 
@@ -11,6 +12,7 @@ public class IndexController {
 
 	@Inject private Result result;
 	
+	@Get("/")
 	public void index(){
 		result.use(Results.http()).body("oi").setStatusCode(200);
 	}
