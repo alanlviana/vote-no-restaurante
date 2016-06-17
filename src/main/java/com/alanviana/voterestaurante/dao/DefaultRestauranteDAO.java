7ee27e80425ac2741dc03ff0d1fc432850d786d9
Alan Luiz Viana <alanlviana@hotmail.com>
2016-06-16 21:29:00 -0300

    Testar Scopo de Request

--- src/main/java/com/alanviana/voterestaurante/dao/DefaultRestauranteDAO.java
+++ src/main/java/com/alanviana/voterestaurante/dao/DefaultRestauranteDAO.java
@@ -2,6 +2,7 @@ package com.alanviana.voterestaurante.dao;
 
 import java.util.List;
 
+import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Default;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
@@ -9,6 +10,7 @@ import javax.persistence.EntityManager;
 import com.alanviana.voterestaurante.model.Restaurante;
 
 @Default
+@RequestScoped
 public class DefaultRestauranteDAO implements RestauranteDAO{
 
 	private final EntityManager entityManager;
