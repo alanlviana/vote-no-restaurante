42f8aa8134c05458a27c24a80c4c876cc9cae3e1
Alan Luiz Viana <alanlviana@hotmail.com>
2016-06-19 14:15:23 -0300

    Teste

--- src/main/java/com/alanviana/voterestaurante/model/Questionario.java
+++ src/main/java/com/alanviana/voterestaurante/model/Questionario.java
@@ -5,10 +5,14 @@ import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
+
 
 @Entity
 public class Questionario implements Serializable{
@@ -26,6 +30,8 @@ public class Questionario implements Serializable{
 	private String nome;
 	@NotNull
 	private String email;	
+	
+	@OneToMany(mappedBy = "questionario", targetEntity = Voto.class, fetch = FetchType.LAZY, cascade = CascadeType.ALL)
 	private List<Voto> votos;
 
 	public Questionario(){}
